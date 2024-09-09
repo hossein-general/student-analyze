c85d03f0d5d99f42c813f9dd396c88e943fa0316
Hossein Ramezani <wave1221ram@gmail.com>
2024-09-09 16:02:53 +0330

    Adding Classes and the main module (without any modification)

---
+++ student_analyze/Persons.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractclassmethod  # Not yet sure how to make the class
+
+
+class BasePerson(ABC):
+    pass
+
+
+class Student(BasePerson):
+    pass
+
+
+class Teacher(BasePerson):
+    pass
---
+++ student_analyze/School.py
@@ -0,0 +1,34 @@
+class School:
+    pass
+
+
+class ClassRoom:
+    pass
+
+
+class SchoolType:
+    pass
+
+
+class EducationGroup:
+    pass
+
+
+class GroupLesson:
+    pass
+
+
+class EducationGrade:
+    pass
+
+
+class Class:
+    pass
+
+
+class ClassSession:
+    pass
+
+
+class ClassGropu:
+    pass

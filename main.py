fbd06a67cf700685eece8425f85000f2ba1a0eac
Hossein Ramezani <wave1221ram@gmail.com>
2024-09-14 08:58:21 +0330

    importing all necessary modules into main module

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from student_analyze import Student, Teacher
+from student_analyze import (
+    Student,
+    Teacher,
+    SchoolType,
+    EducationGrade,
+    EducationGroup,
+    Lesson,
+    ClassSession,
+    ClassSchedule,
+    ClassGroup,
+    ClassRoom,
+    School,
+)
 
 
 def main():
     pass
 
+
 if __name__ == "__main__":
     main()
-
-    
-
-
-

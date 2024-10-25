6a24c1622d40054c37032818ba7ae1149558ec9d
Hossein Ramezani <wave1221ram@gmail.com>
2024-10-25 10:57:16 +0330

    moving all school module classes into the school class and turning them into nested classes [school_module_dev]

--- student_analyze/School.py
+++ student_analyze/School.py
@@ -6,6 +6,8 @@ from .Organization import Organization
 
 # region School
 # School class
+
+
 class School(Organization):
     # last_classroom
     def __init__(self, name, education_state):
@@ -27,7 +29,7 @@ class School(Organization):
             # Getting an id for the new classroom
             classroom_id = self.get_id("classroom")
             # Creating an instance of ClassRoom
-            new_classroom = ClassRoom(self, classroom_id, item)
+            new_classroom = self.ClassRoom(self, classroom_id, item)
             # Assigning the newly created classroom to classrooms dict
             self.classrooms[classroom_id] = new_classroom
 
@@ -53,7 +55,7 @@ class School(Organization):
         # Getting an id for the new classgroup
         classgroup_id = self.get_id("classgroup")
         # Creating an instance of ClassGroup
-        new_classgroup = ClassGroup(
+        new_classgroup = self.ClassGroup(
             self,
             educationgrade,
             student_ids_list,
@@ -75,7 +77,7 @@ class School(Organization):
         # Getting an id for the student from get_id
         student_id = self.get_id("student")
         # Calling the Student class to create an instance of Student
-        new_student = Student(
+        new_student = self.Student(
             self,
             first_name=student_first_name,
             last_name=student_last_name,
@@ -109,116 +111,121 @@ class School(Organization):
 
     def __str__(self):
         return self.name
+    
+
+    
+    # region C-Room
+    # Classroom class which are called by School class instances. they are used as the place for holding class sessions
+    class ClassRoom:
+        __slots__ = ["parent_school", "id", "name"]
+
+        def __init__(self, parent_school, id, name):
+            self.id = id
+            self.parent_school = parent_school
+            self.name = name
+
+        def __str__(self):
+            return f"[{self.id}: {self.name}]"
+
+    # endregion
+
+
+    # region C-Group
+    # Class Group class. its used to bind teachers, students, lessons, classrooms and adding schedule for each
+    # I dont think if there would ever be a need to name a ClassGroup as its not user readable
+    class ClassGroup:
+        def __init__(
+            self,
+            parent_school,
+            educationgrade,
+            student_list: set,
+            teacher_list: set,
+            lessons_list: set,
+            default_classroom,
+            id,
+        ):
+            self.parent_school = parent_school
+            self.educationgrade = educationgrade
+            self.students = student_list
+            self.teacher_list = teacher_list
+            self.lessons_list = lessons_list
+            self.classschedules = set()
+            self.default_classroom = default_classroom
+
+        # Creating a class schedule
+        def add_classschedule(self, classschedule_id, student_list, teacher, lesson):
+            # TODO Getting classschedule_name and removing from parameters
+            # TODO Getting students_list and removing from parameters
+            # TODO Getting teacher and removing from parameters
+            # TODO Getting lesson and removing from parameters
+            new_classschedule = self.ClassSchedule(
+                self, classschedule_id, student_list, teacher, lesson
+            )
+            self.classschedules.add(new_classschedule)
+
+        def __str__(self):
+            return self.id
+
+    # endregion
+
+    # region C-Schedule
+    # This class shows which of the ClassGroup teachers is assigend and what lesson is he presenting (as a sible ClassGroup could hold many teachers and lessons at the same time)
+    # In most cases the ClassSchedule instances are named by the lesson presenting within them, and sometimes combined with an id
+    # e.g. "Riazi-1 20341", "Arabi", ...
+
+
+    class ClassSchedule:
+        def __init__(self, parent_classgroup, id, student_list, teacher, lesson):
+            self.id = id
+            self.student_list = student_list
+            self.teacher = teacher
+            self.lesson = lesson
+            self.classsessions = set()
+
+        def add_classsession(self, start_session, end_session, student_list, teacher):
+            # TODO Getting start_session and removing from parameters
+            # TODO Getting end_session and removing from parameters
+            # TODO Inhariting teacher
+            # TODO Inhariting student_list
+            # TODO Inhariting lesson
+            new_classsession = self.ClassSession(
+                self, start_session, end_session, student_list, teacher
+            )
+            self.classsessions.add(new_classsession)
+
+        def __str__(self):
+            return f"{self.lesson}-{self.id}"
+
+    # endregion
+
+
+    # region C-Sessions
+    # TODO The ClassSession class should be inherited from the ClassSchedule class so it will be containing infrmations like students list, teacher, lesson and etc by default
+    # ClassSessions are instances that contain classSchedule information plus date & time information and is used for a single session
+    # This class can be used for checking presence and absence of each studing within each session
+    # Each ClassSession should only have one teacher
+    class ClassSession:
+        def __init__(
+            self,
+            parent_classsession,
+            start_time,
+            end_time,
+            student_list,
+            teacher,
+            lesson,
+        ):
+            self.parent_classsession = parent_classsession
+            self.start_time = start_time
+            self.end_time = end_time
+            self.student_list = student_list
+            self.teacher = teacher
+            self.lesson = lesson
+
+        def __str__(self):
+            return f"{self.lesson} {self.start_time}-{self.end_time}"
+    # endregion
+
 
 # endregion
 
 
-# region C-Room
-# Classroom class which are called by School class instances. they are used as the place for holding class sessions
-class ClassRoom:
-    __slots__ = ["parent_school", "id", "name"]
-
-    def __init__(self, parent_school, id, name):
-        self.id = id
-        self.parent_school = parent_school
-        self.name = name
-
-    def __str__(self):
-        return f"[{self.id}: {self.name}]"
-
-# endregion
-
-
-# region C-Group
-# Class Group class. its used to bind teachers, students, lessons, classrooms and adding schedule for each
-# I dont think if there would ever be a need to name a ClassGroup as its not user readable
-class ClassGroup:
-    def __init__(
-        self,
-        parent_school,
-        educationgrade,
-        student_list: set,
-        teacher_list: set,
-        lessons_list: set,
-        default_classroom,
-        id,
-    ):
-        self.parent_school = parent_school
-        self.educationgrade = educationgrade
-        self.students = student_list
-        self.teacher_list = teacher_list
-        self.lessons_list = lessons_list
-        self.classschedules = set()
-        self.default_classroom = default_classroom
-
-    # Creating a class schedule
-    def add_classschedule(self, classschedule_id, student_list, teacher, lesson):
-        # TODO Getting classschedule_name and removing from parameters
-        # TODO Getting students_list and removing from parameters
-        # TODO Getting teacher and removing from parameters
-        # TODO Getting lesson and removing from parameters
-        new_classschedule = ClassSchedule(
-            self, classschedule_id, student_list, teacher, lesson
-        )
-        self.classschedules.add(new_classschedule)
-
-    def __str__(self):
-        return self.id
-
-# endregion
-
-# region C-Schedule
-# This class shows which of the ClassGroup teachers is assigend and what lesson is he presenting (as a sible ClassGroup could hold many teachers and lessons at the same time)
-# In most cases the ClassSchedule instances are named by the lesson presenting within them, and sometimes combined with an id
-# e.g. "Riazi-1 20341", "Arabi", ...
-class ClassSchedule:
-    def __init__(self, parent_classgroup, id, student_list, teacher, lesson):
-        self.id = id
-        self.student_list = student_list
-        self.teacher = teacher
-        self.lesson = lesson
-        self.classsessions = set()
-
-    def add_classsession(self, start_session, end_session, student_list, teacher):
-        # TODO Getting start_session and removing from parameters
-        # TODO Getting end_session and removing from parameters
-        # TODO Inhariting teacher
-        # TODO Inhariting student_list
-        # TODO Inhariting lesson
-        new_classsession = ClassSession(
-            self, start_session, end_session, student_list, teacher
-        )
-        self.classsessions.add(new_classsession)
-
-    def __str__(self):
-        return f"{self.lesson}-{self.id}"
-
-# endregion
-
-
-
-# region C-Sessions
-# TODO The ClassSession class should be inherited from the ClassSchedule class so it will be containing infrmations like students list, teacher, lesson and etc by default
-# ClassSessions are instances that contain classSchedule information plus date & time information and is used for a single session
-# This class can be used for checking presence and absence of each studing within each session
-# Each ClassSession should only have one teacher
-class ClassSession:
-    def __init__(
-        self,
-        parent_classsession,
-        start_time,
-        end_time,
-        student_list,
-        teacher,
-        lesson,
-    ):
-        self.parent_classsession = parent_classsession
-        self.start_time = start_time
-        self.end_time = end_time
-        self.student_list = student_list
-        self.teacher = teacher
-        self.lesson = lesson
-
-    def __str__(self):
-        return f"{self.lesson} {self.start_time}-{self.end_time}"
-# endregion
